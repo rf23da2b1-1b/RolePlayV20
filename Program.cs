7b6ab626d95a8a07fecdfbd66e20512250606f0b
Peter Levinsky <pele@zealand.dk>
2023-10-13 10:11:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+NumberGenerator theNumberGenerator = new NumberGenerator();
+BattleLog theLog = new BattleLog();
+
+Hero theHero = new Hero(theNumberGenerator, theLog);
+Beast theBeast = new Beast(theNumberGenerator, theLog);
+
+// Now battle...How do we do that (Hint: You need a loop)
+while (!theHero.Dead && !theBeast.Dead)
+{
+    theHero.ReceiveDamage(theBeast.DealDamage());
+    theBeast.ReceiveDamage(theHero.DealDamage());
+}
+
+
+theLog.PrintLog();
